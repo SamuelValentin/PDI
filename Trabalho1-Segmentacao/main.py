a1ed333f4d074e0dca9c139dcb9dd121b78ea006
SamuelValentin <samuellv89@gmail.com>
2022-08-29 15:16:58 -0300

    Trabalho 1 funcionando e quase concluido

--- Trabalho1-Segmentacao/main.py
+++ Trabalho1-Segmentacao/main.py
@@ -1,7 +1,8 @@
 #===============================================================================
 # Exemplo: segmentação de uma imagem em escala de cinza.
 #-------------------------------------------------------------------------------
-# Autor: Bogdan T. Nassu
+# Professor: Bogdan T. Nassu
+# Alunos: Samuel Valentin e Yan Pinheiro
 # Universidade Tecnológica Federal do Paraná
 #===============================================================================
 
@@ -21,14 +22,14 @@ INPUT_IMAGE =  'arroz.bmp'
 # TODO: ajuste estes parâmetros!
 NEGATIVO = False
 THRESHOLD = 0.8
-ALTURA_MIN = 1
-LARGURA_MIN = 1
-N_PIXELS_MIN = 1
+ALTURA_MIN = 10
+LARGURA_MIN = 10
+N_PIXELS_MIN = 10
 
 #===============================================================================
 
 def binariza (img, threshold):
-    img = np.where(img < threshold, 0, 0.99)
+    img = np.where(img < threshold, 0.0, 1.0)
     return img
     
 #     ''' Binarização simples por limiarização.
@@ -46,27 +47,70 @@ def binariza (img, threshold):
 #-------------------------------------------------------------------------------
 
 def rotula (img, largura_min, altura_min, n_pixels_min):
-    componentes = 0
-    label = 0
+    result = []
+    auxRes = []
+
+    label = 1
+    nPixels = 0
+    topCord =  float('inf')
+    leftCord = float('inf')
+    botCord = -1 
+    rightCord = -1
     
-    mask = img
-    mask = np.where(mask == 0.99, 0, -1)
- 
-    # height, width, number of channels in image
+    #Rotulamento inicial, -1 e 0
+    img = np.where(img == 1.0, -1, 0)
+    
+    print("Flood Fill..."+'\n')
     height = img.shape[0]
     width = img.shape[1]
-    
+
     for i in range(height):
         for j in range(width):
-            if(img[i,j] == 0.99 and mask[i,j] == -1):
-                label = (label + 1)
-                # mask[i,j] = label 
-                floodfillRec(mask, label, i, j)
-               
-    # print(mask)
-    print(label)
+            if img[i,j] == -1:
+                floodfillRec(img,label,i,j)
+                label = label + 1
+
+    print("Dict building..."+'\n')
+    for lab in range(1,label):
+        newDict = {
+                "label": lab,
+                "n_pixels": nPixels,
+                "T": topCord,
+                "L": leftCord,
+                "B": botCord,
+                "R": rightCord,
+        }
+        auxRes.append(newDict)
+        
+    #Percorre cada pixel
+    for i in range(height):
+        for j in range(width):
+            if img[i,j] > 0:
+                #Encontra o correspondente
+                c = auxRes[int(img[i,j])-1] 
+                #Faz o update no contador de pixel e se necessário nos pontos cardiais 
+                c['n_pixels'] = c['n_pixels'] + 1
+                if(j < c['L']):
+                    c['L'] = j
+                if(j > c['R']):
+                    c['R'] = j
+                if(i < c['T']):
+                    c['T'] = i
+                if(i > c['B']):
+                    c['B'] = i
+                auxRes[int(img[i,j])-1] = c
     
-    return componentes 
+    #Percorre cada componente no vetor auxiliar
+    #Se o componente satisfazer o critério minimo de tamanho, altura e largura
+    #Vai ser adicionado no vetor de resultado 
+    for comp in auxRes:
+        if comp['n_pixels'] >= n_pixels_min:
+            if comp['R'] - comp['L'] >= largura_min:
+                 if comp['B'] - comp['T'] >= altura_min:
+                    result.append(comp)
+                    
+    return result
+
 
     # '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
     # [0.1,0.2,etc].
@@ -89,19 +133,20 @@ def rotula (img, largura_min, altura_min, n_pixels_min):
 
 #===============================================================================
 
-def floodfillRec(img, label, y, x):
-    img[y,x] = label
+def floodfillRec(img, label, x, y):
     
-    i = y - 1
-    j = x - 1
-    
-    #visita vizinho
-    for i in range(3):
-        for j in range(3):
-            if((img[i,j] == -1) and  (i >= 0) and (j >= 0)):
-                floodfillRec(img, label, i, j)
-            
+    img[x,y] = label
 
+    for i in range(-1, 3, 2):
+        if (img[x][y+i] != None):
+            if img[x][y+i] == -1:
+                floodfillRec(img, label, x, y+i)    
+                
+    for j in range(-1, 3, 2):
+        if (img[x+j][y] != None):
+            if img[x+j][y] == -1:
+                floodfillRec(img, label, x+j, y)
+    
 def main ():
 
     # Abre a imagem em escala de cinza.
@@ -127,19 +172,17 @@ def main ():
 
     start_time = timeit.default_timer ()
     componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-    # n_componentes = len (componentes)
-    # print ('Tempo: %f' % (timeit.default_timer () - start_time))
-    # print ('%d componentes detectados.' % n_componentes)
+    n_componentes = len (componentes)
+    print ('Tempo: %f' % (timeit.default_timer () - start_time))
+    print ('%d componentes detectados.' % n_componentes)
 
-    # # Mostra os objetos encontrados.
-    # for c in componentes:
-    #     cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+    # Mostra os objetos encontrados.
+    for c in componentes:
+        cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
 
-    # cv2.imshow ('02 - out', img_out)
-    # cv2.imwrite ('02 - out.png', img_out*255)
+    cv2.imshow ('02 - out', img_out)
+    cv2.imwrite ('02 - out.png', img_out*255)
     # cv2.waitKey ()
-    
-    print(componentes)
     cv2.destroyAllWindows ()
 
 
