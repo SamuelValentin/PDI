20e1d2d5e5d2762fa87d90507d0126c33f3405c1
SamuelValentin <samuellv89@gmail.com>
2022-09-22 11:03:57 -0300

    versao antiga do projeto

--- Trabalho3-Bloom/mainYan.py
+++ Trabalho3-Bloom/mainYan.py
@@ -7,14 +7,14 @@ import numpy as np
 
 #=================================================================
 
-INPUT_IMAGE =  "GT2.bmp"
+INPUT_IMAGE =  "GT2.BMP"
 INPUT_IMAGE = os.path.join(sys.path[0], INPUT_IMAGE)
 
 img = cv2.imread(INPUT_IMAGE)
 img = img.astype (np.float32) / 255
 
 imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-threshold = 0.6
+threshold = 0.5
 
 imgHLS[:,:,1] = np.where(imgHLS[:,:,1] > threshold, imgHLS[:,:,1]/3, 0.0)
 im_output = cv2.cvtColor(imgHLS, cv2.COLOR_HLS2BGR)
@@ -25,7 +25,7 @@ vBlur = [im_output, im_output, im_output]
 
 
 for i in range(3):
-    sigma = 5*(3**i)
+    sigma = 5*(2**i)
     v[i] = cv2.GaussianBlur(v[i], (0, 0), sigma)
 
 window_sizes = ((7, 5, 3), (25, 11, 3), (91, 51, 3))
@@ -45,9 +45,6 @@ cv2.imshow('original', img)
 
 cv2.imshow('BlurSum', blurSum)
 cv2.imwrite ('BlurSum.png', blurSum*255)
-
-cv2.waitKey()
-cv2.destroyAllWindows()
 
 cv2.imshow('GaussianSum', gaussianSum)
 cv2.imwrite ('GaussianSum.png', gaussianSum*255)
