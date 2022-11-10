a298a808b67ba8595c47e74b22599dd537e8516d
SamuelValentin <samuellv89@gmail.com>
2022-11-10 13:54:33 -0300

    Trabalho5 fechado e Trabalho final inciado

--- Trabalho5-ChromaKey/main.py
+++ Trabalho5-ChromaKey/main.py
@@ -1,55 +1,22 @@
 #=================================================
 # Universidade Tecnologica Federal do Parana
-# Aluno: Yan Pietrzak Pinheiro
+# Alunos: Samuel L. Valentin e Yan P. Pinheiro
 #=================================================
 
 import cv2
 import os, sys
 import numpy as np
 import copy
+from scipy import stats
 
-INPUT_IMAGES =  ['6.bmp']
+INPUT_IMAGES =  ['0.bmp','1.bmp','2.bmp','3.bmp','4.bmp','5.bmp','6.bmp','7.bmp','8.bmp']
 
 INPUT_PATH = "GT2.bmp"
 INPUT_PATH = os.path.join(sys.path[0], INPUT_PATH)
 
 
-def remove_bg(img):
+def chroma(img, bg):
 
-    print("Removendo o verde")
-
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
-    
-    green = [62,255,255]
-
-    for x in range(img.shape[0]):      
-        for y in range(img.shape[1]):
-
-            diff = green - img[x][y]
-            
-            dh = diff[0] /22
-            ds = diff[1] /255
-            dv = diff[2] /255
-
-            distance = np.sqrt(dh*dh+ds+dv*dv)
-            dv = abs(dv)
-
-            if distance < 1.03 and (ds <= 0.85 and dv <= 0.85):
-                img[x][y][0] = 0
-                img[x][y][1] = 0
-                img[x][y][2] = 0
-
-            elif distance <= 1.20:
-                dist = 1/distance
-                img[x][y][1] = img[x][y][1]*abs(1 - dist)
-                if ds <= 0.85: 
-                    img[x][y][2] = img[x][y][2]*dist
-        
-    img = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
-    return img
-
-def add_bg(img, bg):
-    
     print("Adicionando BG")
     
     img_final = copy.copy(img)
@@ -59,18 +26,42 @@ def add_bg(img, bg):
     # resize image
     resized = cv2.resize(bg, dim, interpolation = cv2.INTER_AREA)
     
+    img_aux = copy.copy(img)
+
+    img_aux = cv2.cvtColor(img_aux,cv2.COLOR_RGB2HSV)
+    resized = cv2.cvtColor(resized,cv2.COLOR_RGB2HSV)
+    
+    # cor = stats.mode(img)[0]
+    # print(cor)
+    green = [62,255,255]
+
     for x in range(img.shape[0]):      
         for y in range(img.shape[1]):
-            if img[x][y][0] == 0 and img[x][y][1] == 0 and img[x][y][2] == 0:
-                img_final[x][y][0] = resized[x][y][0]
-                img_final[x][y][1] = resized[x][y][1]
-                img_final[x][y][2] = resized[x][y][2]
-            else:
-                img_final[x][y][0] = img[x][y][0]
-                img_final[x][y][1] = img[x][y][1]
-                img_final[x][y][2] = img[x][y][2]
-        
-    return img_final
+
+            diff = green - img_aux[x][y]
+            
+            dh = diff[0] /22
+            ds = diff[1] /255
+            dv = diff[2] /255
+
+            distance = np.sqrt(dh*dh+ds+dv*dv)
+            dv = abs(dv)
+
+            if distance < 1.13 and (ds <= 0.85 and dv <= 0.85):
+                img_aux[x][y][0] = resized[x][y][0]
+                img_aux[x][y][1] = resized[x][y][1]
+                img_aux[x][y][2] = resized[x][y][2]
+
+            # elif distance <= 1.20:
+            #     dist = 1/distance
+            #     img_aux[x][y][1] = resized[x][y][1]*abs(1 - dist)
+            #     if ds <= 0.85: 
+            #         img_aux[x][y][2] = resized[x][y][2]*dist
+            
+    img = cv2.cvtColor(img_aux,cv2.COLOR_HSV2RGB)
+    
+    return img
+
  
 def main():
     for src in INPUT_IMAGES:
@@ -82,9 +73,8 @@ def main():
         input_img_bg = input_img_bg.replace("\\","/")
         bg = cv2.imread(input_img_bg, cv2.IMREAD_COLOR)
         
-        img = remove_bg(copy.copy(img_src))
+        img_final = chroma(copy.copy(img_src), bg)
         
-        img_final = add_bg(img, bg)
         
         print("Mostrando resultados...")
 
